9d9b633df280dcd579823d201b8c8bfd1a7001d7
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-13 16:50:05 +0530

    learning redux toolkit with react js

---
+++ ReduxToolkit-js-react/ReduxToolkit-react/src/features/burger/BurgerView.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from "react-redux";
+import { burger_order } from "./burgerSlice";
+
+const BurgerView = () => {
+  const burgerBuns = useSelector((state) => state.burger.burgerBuns);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Number of burger buns - {burgerBuns}</h2>
+      <button onClick={() => dispatch(burger_order())}>Order Burger</button>
+    </div>
+  );
+};
+
+export default BurgerView;
